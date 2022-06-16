1c2a975914fe32e214c1c5a3b373e24fce367203
raheen12 <106184627+raheen12@users.noreply.github.com>
2022-06-16 07:58:43 -0400

    Add files via upload

---
+++ scripting.sh
@@ -0,0 +1,21 @@
+echo "SCRIPT FOR IP RESOLUTION OF SUBDOMAINS"
+echo 
+echo "enter any domain name"
+read a
+#i am using--khaadi.com-- as my domain name
+echo
+#using sublist3r for domains
+echo "----------------SUBLIST3R------------------"
+sublist3r  -d $a -o domains.txt
+echo 
+echo "-----------IP RESOLUTION OF SUBDOMAINS------------"
+for c in $(cat domains.txt); do
+host $c   
+done
+echo 
+echo "--------------BRUTE HOST OF SUBDOMINS-----------"
+dnsrecon -d $a -D /home/kali/Desktop/subdomains1000.txt -t brt -j /home/kali/Desktop/subip.txt
+echo
+#ip.txt for saving results of hidden ips in file
+
+echo "********--THE END--************"
